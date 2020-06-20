e8991e75253560ab94d51aa26811277c09295f70
Vishnushetty <62662144+Vishnu1401@users.noreply.github.com>
2020-06-20 09:26:42 +0530

    An calculator program

---
+++ calpro.py
@@ -0,0 +1,24 @@
+operation = input('''
+Please type what type of opration u want to do=( +,-,*,/ )>>>''')
+
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
+
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+
+else:
+    print('You have not typed a valid operator, please run the program again.')
